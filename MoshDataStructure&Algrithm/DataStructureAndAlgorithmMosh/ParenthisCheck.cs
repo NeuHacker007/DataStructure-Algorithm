2816c27b00f70265c1ec640ff6620b39bff46a42
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-16 14:24:53 -0400

    --first refactor

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/ParenthisCheck.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/ParenthisCheck.cs
@@ -27,5 +27,36 @@ namespace StackDemo {
 
             return stack.Count == 0;
         }
+        public static bool isValid2 (string input) {
+            if (string.IsNullOrWhiteSpace (input)) throw new ArgumentException ("input string is null or empty");
+            Stack<char> stack = new Stack<char> ();
+
+            foreach (var ch in input) {
+                if (isLeftBracket (ch)) {
+                    stack.Push (ch);
+                }
+                if (isRightBracket (ch)) {
+                    if (stack.Count == 0) return false;
+
+                    
+                    char temp = stack.Pop ();
+                    if (isMatchBracket (ch, temp)) {
+                        return true;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        private static bool isLeftBracket (char ch) {
+            return ch == '(' || ch == '<' || ch == '[' || ch == '{';
+        }
+        private static bool isRightBracket (char ch) {
+            return ch == ')' || ch == '>' || ch == ']' || ch == '}';
+        }
+        private static bool isMatchBracket (char left, char right) {
+            return left == '(' && right == ')' || left == '[' && right == ']' || left == '<' && right == '>' || left == '{' && right == '}';
+        }
     }
 }
--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -5,7 +5,14 @@ namespace DataStructureAndAlgorithmMosh {
         static void Main (string[] args) {
             // System.Console.WriteLine (StringReverse.ReverseString ("abcd hello world !"));
             // System.Console.WriteLine (StringReverse.ReverseString2 ("abcd hello world !"));
-            System.Console.WriteLine (StringReverse.ReverseString2 (null));
+            //System.Console.WriteLine (StringReverse.ReverseString2 (null));
+            System.Console.WriteLine (ValidateParenthis.isValid2 (")("));
+            System.Console.WriteLine (ValidateParenthis.isValid2 ("((1+2"));
+            System.Console.WriteLine (ValidateParenthis.isValid2 ("((1+2)"));
+            System.Console.WriteLine (ValidateParenthis.isValid2 ("((1+2))"));
+            System.Console.WriteLine (ValidateParenthis.isValid2 ("((1+2))}"));
+            System.Console.WriteLine (ValidateParenthis.isValid2 ("((1+2)){}"));
+            System.Console.WriteLine (ValidateParenthis.isValid2 ("((1+2))<>"));
 
         }
     }
