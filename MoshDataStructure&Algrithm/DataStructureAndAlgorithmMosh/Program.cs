07faa034a9603e219c8a44e5401ee21151e1e371
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-16 21:11:18 -0400

    -- implemented the min stack

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -3,19 +3,16 @@ using StackExcercise;
 namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args) {
-            TwoStacksInOneArray array = new TwoStacksInOneArray (10);
 
-            array.Push1 (10);
-            array.Push1 (20);
-            array.Push1 (30);
-            // System.Console.WriteLine (array.isFull1 ());
-            // System.Console.WriteLine (array.isFull2 ());
-            // array.Push2 (40);
-            // array.Push2 (50);
-            System.Console.WriteLine (array.Pop1 ());
-            // System.Console.WriteLine (array.Pop2 ());
-            System.Console.WriteLine(array.isEmpty2());
-            System.Console.WriteLine(array.isFull2());
+            MoshMinImplementation stack = new MoshMinImplementation ();
+
+            stack.Push (5);
+            stack.Push (2);
+            stack.Push (1);
+            stack.Push (10);
+            System.Console.WriteLine (stack.Min ());
+            stack.Pop ();
+            System.Console.WriteLine (stack.Min ());
 
         }
     }
