79a0d004aa30800967cc24193a5ba877a1484de3
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-18 08:30:48 -0400

    --removed the main method test code

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -3,13 +3,7 @@ using QueueExcercise;
 namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args) {
-            Queue<int> queue = new Queue<int> ();
-            queue.Enqueue (10);
-            queue.Enqueue (20);
-            queue.Enqueue (30);
-            queue.Enqueue (40);
-            queue.Enqueue (50);
-            System.Console.WriteLine (ReverseFirstKEleInQueue.ReverseKEleInQueue (queue, 5));
+ 
         }
     }
 }
