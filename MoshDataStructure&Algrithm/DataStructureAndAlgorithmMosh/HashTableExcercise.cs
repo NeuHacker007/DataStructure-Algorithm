e0d14571d0df7ca87565237782c2ee9467230e6c
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-20 19:48:16 -0400

    --implemented the count pairs with K difference

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/HashTableExcercise.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/HashTableExcercise.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 namespace HashTableExcercises {
 
     public class HashTableExcercise {
+
+        //  o(n)
         public static int FindMostRepeatedEle (int[] array) {
             if (array.Length == 0) throw new Exception ("Array is empty");
 
@@ -27,6 +30,45 @@ namespace HashTableExcercises {
 
             }
             return result;
+        }
+        public static int[] TwoSum () {
+            return new int[5];
+        }
+
+        public static int CountPairsWithKDiff (int[] array, int k) {
+
+            HashSet<int> set = new HashSet<int> ();
+
+            foreach (var item in array) {
+                set.Add (item);
+            }
+            int count = 0;
+            foreach (var item in array) {
+                if (set.Contains (item + k)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int CountPairsWithKDiffByMosh (int[] array, int k) {
+            HashSet<int> set = new HashSet<int> ();
+
+            foreach (var item in array) {
+                set.Add (item);
+            }
+
+            int count = 0;
+            foreach (var item in array) {
+                if (set.Contains (item + k)) {
+                    count++;
+                }
+                if (set.Contains (item - k)) {
+                    count++;
+                }
+                set.Remove (item);
+            }
+            return count;
         }
     }
 }
--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -3,8 +3,8 @@ using HashTableExcercises;
 namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args) {
-            int[] tests = new int[7] { 1, 4, 4, 2, 2, 2, 4 };
-            System.Console.WriteLine (HashTableExcercise.FindMostRepeatedEle (tests));
+            int[] tests = new int[7] { 1, 5, 5, 5, 5, 12, 2 };
+            System.Console.WriteLine (HashTableExcercise.CountPairsWithKDiff (tests, 2));
         }
     }
 
