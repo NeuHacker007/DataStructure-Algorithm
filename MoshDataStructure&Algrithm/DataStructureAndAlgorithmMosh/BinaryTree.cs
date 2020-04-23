e2ecf44e5b90c067158cb6de6364fecf8e395df6
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-23 09:43:22 -0400

    implemented couple of operations against binary tree

---
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/BinaryTree.cs
@@ -0,0 +1,140 @@
+using System;
+namespace BinaryTreeDemo {
+
+    public class BinaryTree {
+
+        private Node Root;
+
+        public void Insert (int item) {
+
+            var newNode = new Node (item);
+
+            if (Root == null) {
+                Root = newNode;
+                return;
+            }
+
+            var current = Root;
+
+            /*
+                here we keep infinit loop to loop through the tree. 
+                Unlike the linked list we use node != null to loop.
+                Once we insert sucessfully then we just break out the loop
+                otherwise we keep loop
+            */
+
+            while (true) {
+                if (item < current.value) {
+                    // if the left leaf node is null, we can directly put the new node here
+                    if (current.Left == null) {
+                        current.Left = newNode;
+                        break;
+                    }
+                    // this is used to keep search left children in the tree
+                    current = current.Left;
+                } else if (item > current.value) {
+                    // if the right leaf node is null, we can directly put the new node here
+                    if (current.Right == null) {
+                        current.Right = newNode;
+                        break;
+                    }
+                    // this is used to keep search right children in the tree
+                    current = current.Right;
+                }
+            }
+        }
+
+        public bool Find (int item) {
+            if (Root == null) return false;
+            var current = Root;
+            while (true) {
+                if (current.value == item) return true;
+
+                if (item < current.value) {
+                    if (current.Left == null) break;
+                    current = current.Left;
+                } else if (item > current.value) {
+                    if (current.Right == null) break;
+                    current = current.Right;
+                }
+            }
+
+            return false;
+        }
+
+        public bool FindByMosh (int item) {
+            var current = Root;
+
+            while (current != null) {
+                if (item < current.value) {
+                    current = current.Left;
+                } else if (item > current.value) {
+                    current = current.Right;
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void TraversePreOrder () {
+            TraversePreOrder (Root);
+        }
+        private void TraversePreOrder (Node root) {
+            if (root == null) return;
+            System.Console.WriteLine (root.value);
+            TraversePreOrder (root.Left);
+            TraversePreOrder (root.Right);
+        }
+
+        public void TraverseInOrder () {
+            TraverseInOrder (Root);
+        }
+
+        private void TraverseInOrder (Node root) {
+            if (root == null) return;
+            TraverseInOrder (root.Left);
+            System.Console.WriteLine (root.value);
+            TraverseInOrder (root.Right);
+        }
+
+        public void TraversePostOrder () {
+            TraversePostOrder (Root);
+        }
+
+        private void TraversePostOrder (Node root) {
+            if (root == null) return;
+
+            TraversePostOrder (root.Left);
+            TraversePostOrder (root.Right);
+            System.Console.WriteLine (root.value);
+        }
+
+        public int Height () {
+            return Height (Root);
+        }
+        private int Height (Node root) {
+            if (root == null) return -1;
+
+            // recursively call this Height until no leaf nodes
+            // base case
+            if (root.Left == null && root.Right == null) return 0;
+
+            return 1 + Math.Max (Height (root.Left), Height (root.Right));
+        }
+        private class Node {
+            public int value;
+            public Node Left;
+
+            public Node Right;
+
+            public Node (int value) {
+                this.value = value;
+            }
+
+            public override string ToString () {
+                return "Node=" + value;
+            }
+
+        }
+    }
+}
--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
-using HashTableExcercises;
+using BinaryTreeDemo;
 namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args) {
-            int[] tests = new int[7] { 1, 7, 5, 5, 7, 12, 2 };
-            System.Console.WriteLine (HashTableExcercise.CountPairsWithKDiffByMosh (tests, 2));
-            System.Console.WriteLine(HashTableExcercise.CountPairsWithKDiff(tests, 2));
+            BinaryTree tree = new BinaryTree ();
+            tree.Insert (5);
+            tree.Insert (7);
+            tree.Insert (3);
+            tree.Insert (2);
+            tree.Insert (8);
+            tree.Insert (10);
+            System.Console.WriteLine (tree.Height ());
+            System.Console.WriteLine ("done");
         }
     }
 
