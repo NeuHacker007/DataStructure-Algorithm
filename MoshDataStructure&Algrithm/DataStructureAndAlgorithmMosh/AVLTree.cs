2858c1108e72978deab2d1d8e2f010915a213c7c
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-04-27 09:11:22 -0400

    -- implemented the AVL tree basic

---
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/AVLTree.cs
@@ -0,0 +1,71 @@
+using System;
+namespace AVLTreeDemo {
+    public class AVLTree {
+        private Node Root;
+
+        public void Insert (int value) {
+            var newNode = new Node (value);
+            if (Root == null) {
+                Root = newNode;
+                return;
+            }
+            var current = Root;
+
+            while (true) {
+                if (current.Value < value) {
+                    if (current.Right == null) {
+                        current.Right = newNode;
+                        break;
+                    }
+                    current = current.Right;
+                } else if (current.Value > value) {
+                    if (current.Left == null) {
+                        current.Left = newNode;
+                        break;
+                    }
+
+                    current = current.Left;
+                }
+            }
+
+        }
+
+        public void InsertRecursion (int value) {
+            Root = InsertRecursion (Root, value);
+        }
+
+        private Node InsertRecursion (Node root, int value) {
+            if (root == null) {
+                return new Node (value);
+            }
+            if (root.Value < value) {
+                root.Right = InsertRecursion (root.Right, value);
+            } else if (root.Value > value) {
+                root.Left = InsertRecursion (root.Left, value);
+            }
+            root.Height = Math.Max (GetHeight (root.Left), GetHeight (root.Right)) + 1;
+            return root;
+        }
+
+        private int GetHeight (Node root) {
+            return (root == null) ? -1 : root.Height;
+        }
+
+        private class Node {
+            public int Value;
+            public Node Left;
+            public Node Right;
+
+            public int Height;
+
+            public Node (int value) {
+                this.Value = value;
+            }
+
+            public override string ToString () {
+                return "Node=" + Value;
+            }
+
+        }
+    }
+}
--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections.Generic;
-using BinaryTreeDemo;
+﻿using AVLTreeDemo;
 namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args) {
-            BinaryTree tree = new BinaryTree ();
-            tree.Insert (5);
-            tree.Insert (7);
-            tree.Insert (3);
-            tree.Insert (4);
-            tree.Insert (2);
-            tree.Insert (8);
-            tree.Insert (10);
-            System.Console.WriteLine (tree.Contains(5));
-            System.Console.WriteLine (tree.Contains(20));
+            AVLTree tree = new AVLTree ();
+            tree.InsertRecursion (5);
+            tree.InsertRecursion (7);
+            tree.InsertRecursion (3);
+            tree.InsertRecursion (4);
+            tree.InsertRecursion (2);
+            tree.InsertRecursion (8);
+            tree.InsertRecursion (10);
+            System.Console.WriteLine (tree);
+
         }
     }
 
