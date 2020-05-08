ac51a59623eab22dd784304fc1434a7a44d1cb2d
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-05-08 13:07:34 -0400

    -- added couple of leet code question anwsers

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode1.test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Solution;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    class LeetCode1Tests
+    {
+        [Test]
+        [TestCase(new int[]{ 2, 7, 11, 15 }, 9, new int[] {0, 1})]
+        [TestCase(new int[]{  }, 9, new int[] {})]
+        public void TwoSum_WhenCalled_ReturnIndexArrayAddedToTarget(int[] nums, int target, int[] expected)
+        {
+            var result = LeetCode1.TwoSum(nums, target);
+
+            Assert.That(result, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/LeetCode1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    //Example:
+
+    //Given nums = [2, 7, 11, 15], target = 9,
+
+    //Because nums[0] + nums[1] = 2 + 7 = 9,
+    //return [0, 1].
+
+
+    public class LeetCode1
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+             Dictionary<int, int> dic = new Dictionary<int, int>();
+
+             for (int i = 0; i < nums.Length; i++)
+             {
+                 var temp = target - nums[i];
+                 if (dic.ContainsKey(temp))
+                 {
+                     return new[] {dic[temp], i};
+                 }
+                 else
+                 {
+                     if (!dic.ContainsKey(nums[i]))
+                     {
+                         dic.Add(nums[i], i);
+                     }
+                 }
+             }
+
+             return new int[]{};
+        }
+    }
+}
