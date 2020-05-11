21b683344f249f3908f5abb53d0cd011896059df
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-05-11 15:37:12 -0400

    implemented the LeetCode167 and leetcode 21

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode167.tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    class LeetCode167Tests
+    {
+        [Test]
+        [TestCase(new int[] {2, 7, 11, 15}, 9, new int[] {1, 2})]
+        [TestCase(new int[] {2, 8, 11, 15}, 9, new int[] {})]
+        [TestCase(new int[] {}, 9, new int[] {})]
+
+        public void TwoSumII_WhenCalled_ReturnIndexesThatMadeupSumEqualToTarget(int[] nums, int target, int[] expectedResult)
+        {
+            var result = LeetCode167.TwoSumII(nums, target);
+
+            Assert.That(result, Is.EquivalentTo(expectedResult));
+        }
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/LeetCode167.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class LeetCode167
+    {
+        //Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+        //    The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+        //    Note:
+
+        //Your returned answers (both index1 and index2) are not zero-based.
+        //    You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+        //    Example:
+
+        //Input: numbers = [2, 7, 11, 15], target = 9
+        //Output: [1,2]
+        //Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+
+        public static int[] TwoSumII(int[] nums, int target)
+        {
+            List<int> result = new List<int>();
+
+            int low = 0;
+            int high = nums.Length - 1;
+
+            while (low < high)
+            {
+                var temp = nums[low] + nums[high];
+
+                if (temp == target)
+                {
+                    result.Add(low + 1);
+                    result.Add(high + 1);
+                    break;
+                } else if (temp > target)
+                {
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }
+            }
+
+
+            return result.ToArray();
+        }
+    }
+}
