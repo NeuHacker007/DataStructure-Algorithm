68442f0db9c9d79ec10acd28d1e1ac232ffb92c8
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-05-11 20:26:22 -0400

    implemented LeetCode 15 and leetcode 937

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/LeetCode15.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    public class LeetCode15
+    {
+        public static IList<IList<int>> Get3Sum(int[] nums)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length && nums[i] <= 0; ++i)
+            {
+                if (i == 0 || nums[i - 1] != nums[i])
+                {
+                    TwoSum(nums, i, result);
+                }
+            }
+
+            return result;
+        }
+
+
+        private static void TwoSum(int[] nums, int index, IList<IList<int>> resultList)
+        {
+            // TODO fix the test failure
+            int low = index + 1;
+            int high = nums.Length - 1;
+
+            while (low < high)
+            {
+                int sum = nums[index] + nums[low] + nums[high];
+                if (sum < 0 || low > (index + 1) && nums[low] == nums[low - 1])
+                {
+                    low++;
+                } else if (sum > 0 || high < nums.Length - 1 && nums[high] == nums[high + 1])
+                {
+                    high--;
+                }
+                else
+                {
+                    List<int> temp = new List<int>();
+                    temp.Add(nums[index]);
+                    temp.Add(nums[low++]);
+                    temp.Add(nums[high--]);
+
+                    resultList.Add(temp);
+                }
+            }
+        }
+    }
+}
