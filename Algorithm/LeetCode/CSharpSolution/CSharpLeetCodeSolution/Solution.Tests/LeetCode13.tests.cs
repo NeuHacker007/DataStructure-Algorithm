fb5a8fb0018409378d7ae6d583710f2bf541d4d3
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-05-14 08:45:41 -0400

    implemented LeetCode12/13

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode13.tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    class LeetCode13Tests
+    {
+
+        [Test]
+        [TestCase("III",3)]
+        public void RomanToIntV1_WhenCalled_ReturnIntegerRepresentByRoman(string s, int expectedResult)
+        {
+           var result = LeetCode13.RomanToIntV1(s);
+
+           Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
