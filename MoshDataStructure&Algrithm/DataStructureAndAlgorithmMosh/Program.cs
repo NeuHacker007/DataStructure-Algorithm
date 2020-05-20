8b42f6d45cfbbb65cf81045516b42efe0214c090
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-05-19 22:26:38 -0400

    Implemented the min heap

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -3,18 +3,22 @@ using MaxHeapHeaplifyDemo;
 namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args) {
-            Heap heap = new Heap (6);
-            // heap.Insert (10);
-            // heap.Insert (5);
-            // heap.Insert (17);
-            // heap.Insert (4);
-            // heap.Insert (22);
-            // heap.Remove ();
+            MinHeap heap = new MinHeap (6);
+            heap.Insert (10, "a");
+            heap.Insert (5, "b");
+            heap.Insert (17, "c");
+            heap.Insert (4, "d");
+            heap.Insert (22, "e");
+             heap.Insert (1, "e");
+            heap.Remove ();
+            heap.Remove ();
+            heap.Remove ();
+            System.Console.WriteLine (heap);
             // System.Console.WriteLine ("Done");
-            int[] numbers = new int[] { 5, 3, 8, 4, 1, 2 };
-         //   Maxheap.Heaplify (numbers);
+            //     int[] numbers = new int[] { 5, 3, 8, 4, 1, 2 };
+            //  //   Maxheap.Heaplify (numbers);
 
-            System.Console.WriteLine (heap.IsMaxHeap (numbers));
+            //     System.Console.WriteLine (heap.IsMaxHeap (numbers));
 
             System.Console.WriteLine ("Done");
         }
