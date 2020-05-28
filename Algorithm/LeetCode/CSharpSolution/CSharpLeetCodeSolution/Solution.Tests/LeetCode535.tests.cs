eb9fc0b685fc20dbcb2b679c99b6806a8719e1bf
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-05-27 22:53:29 -0400

    implemented the LeetCode535

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode535.tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    class LeetCode535Tests
+    {
+        [Test]
+        [TestCase("https://leetcode.com/problems/design-tinyurl")]
+        public void Encode_WhenCalled_ReturnUniqueTinyUrl(string longUrl)
+        {
+            var result = LeetCode535.Encode(longUrl);
+
+            Assert.That(result, Does.Match("^http://tinyurl.com/[a-zA-Z0-9]{0,6}"));
+        }
+    }
+}
