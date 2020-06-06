bd39a6e240373dc609b7dd98872ad9ab44978835
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-06-05 22:30:32 -0400

    updated the comments position

--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/LeetCode146.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/LeetCode146.cs
@@ -4,6 +4,30 @@ using System.Text;
 
 namespace Solution
 {
+    //Design and implement a data structure for Least Recently Used(LRU) cache.
+    //It should support the following operations: get and put.
+    //    get(key) - Get the value(will always be positive) of the key if the key exists
+    //               in the cache, otherwise return -1.
+    //    put(key, value) - Set or insert the value if the key is not already
+    //    present.When the cache reached its capacity,
+    //    it should invalidate the least recently used item before inserting a new item.
+    //    The cache is initialized with a positive capacity.
+    //    Follow up:
+    //Could you do both operations in O(1) time complexity?
+
+    //Example:
+
+    //LRUCache cache = new LRUCache(2 /* capacity */ );
+
+    //cache.put(1, 1);
+    //cache.put(2, 2);
+    //cache.get(1);       // returns 1
+    //cache.put(3, 3);    // evicts key 2
+    //cache.get(2);       // returns -1 (not found)
+    //cache.put(4, 4);    // evicts key 1
+    //cache.get(1);       // returns -1 (not found)
+    //cache.get(3);       // returns 3
+    //cache.get(4);       // returns 4
     public class DLinkedDicNode
     {
         public int Key;
@@ -20,30 +44,7 @@ namespace Solution
 
     public class LeetCode146
     {
-        //Design and implement a data structure for Least Recently Used(LRU) cache.
-        //It should support the following operations: get and put.
-        //    get(key) - Get the value(will always be positive) of the key if the key exists
-        //               in the cache, otherwise return -1.
-        //    put(key, value) - Set or insert the value if the key is not already
-        //    present.When the cache reached its capacity,
-        //    it should invalidate the least recently used item before inserting a new item.
-        //    The cache is initialized with a positive capacity.
-        //    Follow up:
-        //Could you do both operations in O(1) time complexity?
-
-        //Example:
-
-        //LRUCache cache = new LRUCache(2 /* capacity */ );
-
-        //cache.put(1, 1);
-        //cache.put(2, 2);
-        //cache.get(1);       // returns 1
-        //cache.put(3, 3);    // evicts key 2
-        //cache.get(2);       // returns -1 (not found)
-        //cache.put(4, 4);    // evicts key 1
-        //cache.get(1);       // returns -1 (not found)
-        //cache.get(3);       // returns 3
-        //cache.get(4);       // returns 4
+    
         private Dictionary<int, DLinkedDicNode> cache = new Dictionary<int, DLinkedDicNode>();
         private int Size;
         private int Capacity;
