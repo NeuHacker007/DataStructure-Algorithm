376aa29b5b1222119c47e5e79085df85fc49d8d0
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-13 14:46:46 -0400

    Implemented leetcode 560 & its unit tests

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode560.tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    public class LeetCode560Tests
+    {
+        [Test]
+        [TestCase(new int[] { 1, 1, 1 }, 2, 2)]
+        [TestCase(new int[] { 1, 1, 1 }, 1, 3)]
+        [TestCase(new int[] { 1, 1, 1 }, 3, 1)]
+        [TestCase(new int[] { 1, 1, 1 }, 0, 0)]
+        public void SubArraySumBruteForce_ReturnCount(int[] nums, int k, int expectedResult)
+        {
+            var result = LeetCode560.SubArraySumBruteForce(nums, k);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 1 }, 2, 2)]
+        [TestCase(new int[] { 1, 1, 1 }, 1, 3)]
+        [TestCase(new int[] { 1, 1, 1 }, 3, 1)]
+        [TestCase(new int[] { 1, 1, 1 }, 0, 0)]
+        public void SubArraySumCumulativeSum_ReturnCount(int[] nums, int k, int expectedResult)
+        {
+            var result = LeetCode560.SubArraySumBruteForce(nums, k);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 1 }, 2, 2)]
+        [TestCase(new int[] { 1, 1, 1 }, 1, 3)]
+        [TestCase(new int[] { 1, 1, 1 }, 3, 1)]
+        [TestCase(new int[] { 1, 1, 1 }, 0, 0)]
+        public void SubArraySumWithoutSpace_ReturnCount(int[] nums, int k, int expectedResult)
+        {
+            var result = LeetCode560.SubArraySumBruteForce(nums, k);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 1, 1 }, 2, 2)]
+        [TestCase(new int[] { 1, 1, 1 }, 1, 3)]
+        [TestCase(new int[] { 1, 1, 1 }, 3, 1)]
+        [TestCase(new int[] { 1, 1, 1 }, 0, 0)]
+        public void SubArraySumHashMap_ReturnCount(int[] nums, int k, int expectedResult)
+        {
+            var result = LeetCode560.SubArraySumBruteForce(nums, k);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
