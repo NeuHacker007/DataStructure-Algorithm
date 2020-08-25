e8c2c4040e36f38b90d7f05e71c89580e2133e68
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-24 22:17:28 -0400

    Implemented LeetCode142

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L142.Linked.List.Cycle.II.Medium/LeetCode142.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution.L142.Linked.List.Cycle.II.Medium
+{
+    public class LeetCode142Node
+    {
+        public int val;
+        public LeetCode142Node next;
+        public LeetCode142Node(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+    public class LeetCode142
+    {
+
+        public static LeetCode142Node DetectCircleByTwoPointer(LeetCode142Node head)
+        {
+            if (head?.next == null) return null;
+
+            var slow = head;
+            var fast = head;
+
+            while (fast?.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                {
+                    slow = head;
+
+                    while (slow != fast)
+                    {
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+
+                    return slow;
+                }
+            }
+
+            return null;
+        }
+
+        public static LeetCode142Node DetectCircleHashMap(LeetCode142Node head)
+        {
+
+            var hashMap = new HashSet<LeetCode142Node>();
+
+            while (head != null)
+            {
+                if (!hashMap.Contains(head))
+                {
+                    hashMap.Add(head);
+                    head = head.next;
+                }
+                else
+                {
+                    return head;
+                }
+
+
+            }
+
+            return null;
+        }
+    }
+}
