1d4ccbb7e5ca758695c89e5f494cfad31d292110
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-09-09 21:15:49 -0400

    modified the using statement

--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/Program.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/Program.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Solution.L509_Fibonacci_Num_Easy;
+using Solution;
 
-namespace Solution
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(LeetCode509.GetFibonacciNumDPEquation(3));
+namespace Solution {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine (LeetCode509.GetFibonacciNumDPEquation (3));
 
-            Console.ReadKey();
+            Console.ReadKey ();
         }
     }
 }
