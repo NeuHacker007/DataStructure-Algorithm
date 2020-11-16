4241a31c18be6a21cb164c1583939a79b7f071d3
Ivan Zhang <yf.eva.yifan@gmail.com>
2020-11-16 16:13:09 -0500

    Refactored the folder structure

--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/Program.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Solution;
 
-namespace Solution {
-    class Program {
-        static void Main (string[] args) {
-            Console.WriteLine (LeetCode509.GetFibonacciNumDPEquation (3));
-
-            Console.ReadKey ();
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
         }
     }
 }
