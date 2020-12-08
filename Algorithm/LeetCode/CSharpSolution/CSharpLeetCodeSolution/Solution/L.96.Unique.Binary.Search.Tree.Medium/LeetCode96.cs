99d36c86913fea9203390e2af5cc1b1cb3608674
Ivan Zhang <yf.eva.yifan@gmail.com>
2020-12-08 15:07:50 -0500

    Fixed the parenthis issue

--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.96.Unique.Binary.Search.Tree.Medium/LeetCode96.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.96.Unique.Binary.Search.Tree.Medium/LeetCode96.cs
@@ -1,7 +1,7 @@
 /**
  * Author: yf.eva.yifan@gmail.com
  * Date: 12-04-2020 14:05:26
- * LastEditTime: 12-04-2020 14:10:39
+ * LastEditTime: 12-08-2020 15:07:29
  * FilePath: \CSharpLeetCodeSolution\Solution\L.96.Unique.Binary.Search.Tree.Medium\LeetCode96.cs
  * Description: 
  */
@@ -9,18 +9,23 @@
 
 namespace Solution
 {
-    public static int NumTrees(int n) {
-        in[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+    public class LeetCode96
+    {
+        public static int NumTrees(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
 
-        for(int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                dp[i] += dp[j - 1] * dp[j - i];
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    dp[i] += dp[j - 1] * dp[j - i];
+                }
             }
-        }
 
-        return dp[n];
+            return dp[n];
+        }
     }
-    
 }
