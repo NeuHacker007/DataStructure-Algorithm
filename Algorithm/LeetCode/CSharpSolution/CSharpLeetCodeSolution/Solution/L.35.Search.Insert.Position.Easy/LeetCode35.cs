b458eb32190de938246fe8e9b713284740cd4c46
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-05-14 09:38:54 -0400

    Implemented lc 35

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.35.Search.Insert.Position.Easy/LeetCode35.cs
@@ -0,0 +1,40 @@
+/**
+ * Author: yf.eva.yifan@gmail.com
+ * Date: 05-14-2021 09:27:57
+ * LastEditTime: 05-14-2021 09:33:45
+ * FilePath: \CSharpLeetCodeSolution\Solution\L.35.Search.Insert.Position.Easy\LeetCode35.cs
+ * Description: 
+ */
+
+namespace LcArraySolution
+{
+
+    public class LeetCode35
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var low = 0;
+            var high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                var mid = (high - low) / 2 + low;
+
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                else if (target < nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+
+            }
+            return low;
+        }
+    }
+}
