30d29e4a42a35c590a0fc234159e7932923d0856
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-05-26 15:21:27 -0400

    Updated the Leetcode 654

--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.654.Maximum.Binary.Tree.Medium/LeetCode654.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.654.Maximum.Binary.Tree.Medium/LeetCode654.cs
@@ -1,19 +1,19 @@
 /**
  * Author: yf.eva.yifan@gmail.com
  * Date: 05-18-2021 11:08:47
- * LastEditTime: 05-18-2021 12:37:17
+ * LastEditTime: 05-26-2021 15:20:44
  * FilePath: \CSharpLeetCodeSolution\Solution\L.654.Maximum.Binary.Tree.Medium\LeetCode654.cs
  * Description: 
  */
 using System;
 namespace LcTreeSolution
 {
-    public class TreeNode
+    public class LeetCode549TreeNode
     {
         public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        public LeetCode549TreeNode left;
+        public LeetCode549TreeNode right;
+        public LeetCode549TreeNode(int val = 0, LeetCode549TreeNode left = null, LeetCode549TreeNode right = null)
         {
             this.val = val;
             this.left = left;
@@ -22,13 +22,13 @@ namespace LcTreeSolution
     }
     public class LeetCode654
     {
-        public static TreeNode ConstructMaximumBinaryTree(int[] nums)
+        public static LeetCode549TreeNode ConstructMaximumBinaryTree(int[] nums)
         {
             return Build(nums, 0, nums.Length - 1);
         }
 
         // return [lo .. hi] tree 
-        private static TreeNode Build(int[] nums, int lo, int hi)
+        private static LeetCode549TreeNode Build(int[] nums, int lo, int hi)
         {
             if (lo > hi) return null;
 
@@ -44,7 +44,7 @@ namespace LcTreeSolution
                 }
             }
 
-            var root = new TreeNode(max);
+            var root = new LeetCode549TreeNode(max);
 
             root.left = Build(nums, lo, index - 1);
             root.right = Build(nums, index + 1, hi);
