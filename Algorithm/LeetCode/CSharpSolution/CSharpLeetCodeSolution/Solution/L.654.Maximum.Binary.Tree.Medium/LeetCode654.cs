70b29e9461ce777f4e0137df7937c4b2a5b6ef8f
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-05-26 15:25:46 -0400

    udpated the class name

--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.654.Maximum.Binary.Tree.Medium/LeetCode654.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.654.Maximum.Binary.Tree.Medium/LeetCode654.cs
@@ -1,19 +1,19 @@
 /**
  * Author: yf.eva.yifan@gmail.com
  * Date: 05-18-2021 11:08:47
- * LastEditTime: 05-26-2021 15:20:44
+ * LastEditTime: 05-26-2021 15:25:20
  * FilePath: \CSharpLeetCodeSolution\Solution\L.654.Maximum.Binary.Tree.Medium\LeetCode654.cs
  * Description: 
  */
 using System;
 namespace LcTreeSolution
 {
-    public class LeetCode549TreeNode
+    public class LeetCode654TreeNode
     {
         public int val;
-        public LeetCode549TreeNode left;
-        public LeetCode549TreeNode right;
-        public LeetCode549TreeNode(int val = 0, LeetCode549TreeNode left = null, LeetCode549TreeNode right = null)
+        public LeetCode654TreeNode left;
+        public LeetCode654TreeNode right;
+        public LeetCode654TreeNode(int val = 0, LeetCode654TreeNode left = null, LeetCode654TreeNode right = null)
         {
             this.val = val;
             this.left = left;
@@ -22,13 +22,13 @@ namespace LcTreeSolution
     }
     public class LeetCode654
     {
-        public static LeetCode549TreeNode ConstructMaximumBinaryTree(int[] nums)
+        public static LeetCode654TreeNode ConstructMaximumBinaryTree(int[] nums)
         {
             return Build(nums, 0, nums.Length - 1);
         }
 
         // return [lo .. hi] tree 
-        private static LeetCode549TreeNode Build(int[] nums, int lo, int hi)
+        private static LeetCode654TreeNode Build(int[] nums, int lo, int hi)
         {
             if (lo > hi) return null;
 
@@ -44,7 +44,7 @@ namespace LcTreeSolution
                 }
             }
 
-            var root = new LeetCode549TreeNode(max);
+            var root = new LeetCode654TreeNode(max);
 
             root.left = Build(nums, lo, index - 1);
             root.right = Build(nums, index + 1, hi);
