879128f7386302925eab701f06c9d44ff6e0f22d
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-10-14 21:42:09 -0400

    Implemented the leetcode88

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.88.Merge.Sorted.Array.Easy/LeetCode88.cs
@@ -0,0 +1,56 @@
+/**
+ * Author: yf.eva.yifan@gmail.com
+ * Date: 10-14-2021 21:19:37
+ * LastEditTime: 10-14-2021 21:38:45
+ * FilePath: \CSharpLeetCodeSolution\Solution\L.88.Merge.Sorted.Array.Easy\LeetCode88.cs
+ * Description: 
+ */
+
+using System.Text;
+namespace ArraySolution
+{
+
+    public static class ExtendMethod
+    {
+        public static string PrintArray(this int[] array)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in array)
+            {
+                sb.Append(item);
+                sb.Append(",");
+            }
+
+            return sb.ToString().TrimEnd(',');
+        }
+    }
+
+    public class LeetCode88
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var p1 = m - 1;
+            var p2 = n - 1;
+            var p = m + n - 1;
+            while (p1 > -1 && p2 > -1)
+            {
+
+                if (nums1[p1] > nums2[p2])
+                {
+                    nums1[p--] = nums1[p1--];
+                }
+                else
+                {
+                    nums1[p--] = nums2[p2--];
+                }
+            }
+
+            while (p2 > -1)
+            {
+                nums1[p--] = nums2[p2--];
+            }
+            System.Console.WriteLine(nums1.PrintArray());
+        }
+    }
+}
