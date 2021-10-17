f1dc6ae3ae0695ea1bcb5770aba1c47b9dac205e
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-10-17 15:38:12 -0400

    Refactored the IsMaxHeap function to enhance the readability

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Heap/HeapJudger.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Heap/HeapJudger.cs
@@ -7,27 +7,42 @@ namespace DataStructureAndAlgorithmMosh.Heap
 {
     public class HeapJudger
     {
-        public static bool IsMaxHeap<T>(T[] array)
+        public static bool IsMaxHeap<T>(T[] array) 
         {
             if (array.Length == 0)
             {
                 return false;
             }
-            // int i = 0;
-            //var leftChildIndex = 2 * i + 1;
-            //var rightChildIndex = 2 * i + 2;
             for (var i = 0; i < array.Length; i++)
             {
-                var leftChildIndex = 2 * i + 1;
-                var rightChildIndex = 2 * i + 2;
+                var leftChildIndex = GetLeftChildIndex(i);
+                var rightChildIndex = GetRightChildIndex(i);
                 if (leftChildIndex >= array.Length || rightChildIndex >= array.Length) break;
-                if (Comparer<T>.Default.Compare(array[i], array[leftChildIndex]) < 0) return false;
-                if (Comparer<T>.Default.Compare(array[i], array[rightChildIndex]) < 0) return false;
+                if (IsCurrentLessThanLeftChild(array, i, leftChildIndex)) return false;
+                if (IsCurrentLessThanRightChild(array, i, rightChildIndex)) return false;
             }
 
             return true;
         }
 
+        private static bool IsCurrentLessThanRightChild<T>(T[] array, int i, int rightChildIndex)
+        {
+            return Comparer<T>.Default.Compare(array[i], array[rightChildIndex]) < 0;
+        }
 
+        private static bool IsCurrentLessThanLeftChild<T>(T[] array, int i, int leftChildIndex)
+        {
+            return Comparer<T>.Default.Compare(array[i], array[leftChildIndex]) < 0;
+        }
+
+        private static int GetRightChildIndex(int i)
+        {
+            return 2 * i + 2;
+        }
+
+        private static int GetLeftChildIndex(int i)
+        {
+            return 2 * i + 1;
+        }
     }
 }
--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -5,16 +5,16 @@ namespace DataStructureAndAlgorithmMosh {
     class Program {
         static void Main (string[] args)
         {
-            //var nums = new int[] {5, 3, 10, 1, 4, 2};
+            var nums = new int[] {5, 3, 10, 1, 4, 2};
 
             //HeapSort.ShowHeapSortAsc(numbers);
             //HeapSort.ShowHeapSortDesc(numbers);
 
             //var nums = new int[] {5, 3, 8, 4, 1, 2};
 
-            var nums = new int[]{};
+            //var nums = new int[]{};
 
-            ArrayMaxHeaplify.MaxHeaplify(nums);
+           // ArrayMaxHeaplify.MaxHeaplify(nums);
 
             Console.WriteLine(HeapJudger.IsMaxHeap<int>(nums));
             Console.WriteLine(nums.PrintArray());
