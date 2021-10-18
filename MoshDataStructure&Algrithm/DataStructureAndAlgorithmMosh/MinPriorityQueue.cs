c58b69f70692730691b8479b685347f3337df385
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-10-18 15:36:57 -0400

    Implemented the minHeap based Priority queue

--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/MinPriorityQueue.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/MinPriorityQueue.cs
@@ -1,3 +1,191 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
 namespace HeapDemo {
-    // TODO: Implement with the MinHeap class
+
+    public class MinHeap<T>
+    {
+        private readonly int _capacity;
+        private T[] _elements;
+        private int _count;
+        public int Count => _count;
+        public bool IsFull => _count == _capacity;
+        public bool IsEmpty => _count == 0;
+
+        public MinHeap(int capacity = 10)
+        {
+            _capacity = capacity;
+            _elements = new T[capacity];
+            _count = 0;
+        }
+
+
+        public void Add(T element)
+        {
+            _elements[_count++] = element;
+
+            BubbleUp();
+        }
+
+        private void BubbleUp()
+        {
+            if (IsFull)
+            {
+                throw new Exception("Heap is full");
+            }
+            var index = _count - 1;
+
+            while (index > 0
+                   && Comparer<T>
+                       .Default
+                       .Compare(_elements[index], _elements[GetParentIndex(index)]) < 0)
+            {
+                var parentIndex = GetParentIndex(index);
+                Swap(parentIndex, index);
+
+                index = parentIndex;
+            }
+        }
+
+        private void Swap(int first, int second)
+        {
+            var temp = _elements[first];
+            _elements[first] = _elements[second];
+            _elements[second] = temp;
+        }
+
+
+        public T Remove()
+        {
+            if (IsEmpty) throw new Exception("Heap is empty");
+            var result = _elements[0];
+            _elements[0] = _elements[--_count];
+            BubbleDown();
+
+            return result;
+        }
+
+        private void BubbleDown()
+        {
+            var index = 0;
+
+            while (index <= _count && !IsValidChild(index))
+            {
+                var smallerChildIndex = GetSmallerChildIndex(index);
+                Swap(index, smallerChildIndex);
+                index = smallerChildIndex;
+            }
+        }
+
+        private int GetSmallerChildIndex(int index)
+        {
+            if (!HasLeftChild(index)) return index;
+
+            if (!HasRightChild(index)) return GetLeftChildIndex(index);
+
+            return Comparer<T>.Default.Compare(GetLeftChild(index), GetRightChild(index)) < 0 
+                ? GetLeftChildIndex(index)
+                : GetRightChildIndex(index);
+        }
+
+        private T GetLeftChild(int index)
+        {
+            return _elements[GetLeftChildIndex(index)];
+        }
+
+        private T GetRightChild(int index)
+        {
+            return _elements[GetRightChildIndex(index)];
+        }
+        private bool IsValidChild(int index)
+        {
+            if (!HasLeftChild(index))
+            {
+                return true;
+            }
+
+            if (!HasRightChild(index))
+            {
+                return Comparer<T>.Default.Compare(_elements[index], _elements[GetLeftChildIndex(index)]) <= 0;
+            }
+
+            return Comparer<T>.Default.Compare(_elements[index], _elements[GetLeftChildIndex(index)]) <= 0
+                   && Comparer<T>.Default.Compare(_elements[index], _elements[GetRightChildIndex(index)]) <= 0;
+        }
+
+        private bool HasRightChild(int index)
+        {
+            return GetRightChildIndex(index) <= _count;
+        }
+
+        private bool HasLeftChild(int index)
+        {
+            return GetLeftChildIndex(index) <= _count;
+        }
+
+        private int GetLeftChildIndex(int index)
+        {
+            return 2 * index + 1;
+        }
+
+        private int GetRightChildIndex(int index)
+        {
+            return 2 * index + 2;
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("[");
+
+            for (var i = 0; i < _count; i++)
+            {
+                sb.Append(_elements[i]);
+
+                if (i == _count - 1) break;
+                sb.Append(",");
+            }
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+    }
+
+    public class MinHeapBasedPriorityQueue<T>
+    {
+        private readonly MinHeap<T> _minHeap;
+
+
+        public MinHeapBasedPriorityQueue(int capacity = 10)
+        {
+            _minHeap = new MinHeap<T>(capacity);
+        }
+
+        public void Add(T element)
+        {
+            _minHeap.Add(element);
+        }
+
+        public T Remove()
+        {
+            return _minHeap.Remove();
+        }
+
+        public bool IsEmpty => _minHeap.IsEmpty;
+        public bool IsFull => _minHeap.IsFull;
+        public int Size => _minHeap.Count;
+
+        public override string ToString()
+        {
+            return _minHeap.ToString();
+        }
+    }
 }
--- MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
+++ MoshDataStructure&Algrithm/DataStructureAndAlgorithmMosh/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using TrieDemo;
-using DataStructureAndAlgorithmMosh.Heap;
-using DataStructureAndAlgorithmMosh.PriorityQueue;
+using HeapDemo;
 
 namespace DataStructureAndAlgorithmMosh
 {
@@ -16,28 +15,50 @@ namespace DataStructureAndAlgorithmMosh
 
             //var nums = new int[] {5, 3, 8, 4, 1, 2};
 
-            //var nums = new int[]{};
+           // //var nums = new int[]{};
 
-            // ArrayMaxHeaplify.MaxHeaplify(nums);
+           // // ArrayMaxHeaplify.MaxHeaplify(nums);
 
-            // Console.WriteLine(HeapJudger.IsMaxHeap<int>(nums));
-            //  Console.WriteLine(nums.PrintArray());
+           // // Console.WriteLine(HeapJudger.IsMaxHeap<int>(nums));
+           // //  Console.WriteLine(nums.PrintArray());
+            
+           // queue = new MinHeapBasedPriorityQueue();
+           // queue.Add("Ivan", 5);
+           // queue.Add("Eva", 3);
+           // queue.Add("Guopin", 10);
+           // queue.Add("Tom", 1);
+           // queue.Add("jack", 4);
+           // queue.Add("Baby", 2);
+           // //Console.WriteLine(queue);
+           // //queue.Remove();
+           // //Console.WriteLine(queue);
+           // //queue.Remove();
+           // //Console.WriteLine(queue);
+           //var tuple = queue.Remove();
+           //Console.WriteLine($"Value: {tuple.Item1}; Priority:{tuple.Item2}");
+           //// Console.WriteLine(queue);
+           ///
+           MinHeap<int> minHeap = new MinHeap<int>();
 
-            MinHeapBasedPriorityQueue queue = new MinHeapBasedPriorityQueue();
-            queue.Add("Ivan", 5);
-            queue.Add("Eva", 3);
-            queue.Add("Guopin", 10);
-            queue.Add("Tom", 1);
-            queue.Add("jack", 4);
-            queue.Add("Baby", 2);
-            //Console.WriteLine(queue);
-            //queue.Remove();
-            //Console.WriteLine(queue);
-            //queue.Remove();
-            //Console.WriteLine(queue);
-           var tuple = queue.Remove();
-           Console.WriteLine($"Value: {tuple.Item1}; Priority:{tuple.Item2}");
-           // Console.WriteLine(queue);
+           minHeap.Add(5);
+           minHeap.Add(3);
+           minHeap.Add(8);
+           minHeap.Add(4);
+           minHeap.Add(1);
+           minHeap.Add(2);
+           Console.WriteLine(minHeap);
+           minHeap.Remove();
+           Console.WriteLine(minHeap);
+           minHeap.Remove();
+           Console.WriteLine(minHeap);
+           minHeap.Remove();
+           Console.WriteLine(minHeap);
+           minHeap.Remove();
+           Console.WriteLine(minHeap);
+           minHeap.Remove();
+           Console.WriteLine(minHeap);
+           minHeap.Remove();
+           Console.WriteLine(minHeap);
             Console.ReadLine();
 
 
