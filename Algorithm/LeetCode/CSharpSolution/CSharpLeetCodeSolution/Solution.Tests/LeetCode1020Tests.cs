bc7a9ad6426612621220a28f2aced979977d9c1f
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-12-03 14:50:04 -0500

    Implemented leetcode1020

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode1020Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFSSolution;
+using NUnit.Framework;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    public class LeetCode1020Tests
+    {
+        private static int[][] grid1 = new []
+        {
+            new int[]{0,0,0,0},
+            new int[]{1,0,1,0},
+            new int[]{0,1,1,0},
+            new int[]{0,0,0,0}
+        };
+
+        private static int[][] grid2 = new[]
+        {
+            new int[]{0,1,1,0},
+            new int[]{0,0,1,0},
+            new int[]{0,0,1,0},
+            new int[]{0,0,0,0}
+        };
+
+        private static Object[] numofEnclaves = new Object[]
+        {
+            new Object[]{grid1, 3},
+            new Object[]{grid2, 0}
+        };
+        
+        [Test]
+        [TestCaseSource("numofEnclaves")]
+        public void NumEnclaves_WhenCalled_ReturnNumOfEnclaves(int[][] grid, int expectedResult)
+        {
+            var result = LeetCode1020.NumEnclaves(grid);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.1020.Number.Of.Enclaves.Medium/LeetCode1020.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFSSolution
+{
+    public class LeetCode1020
+    {
+        public static int NumEnclaves(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            for (int row = 0; row < m; row++)
+            {
+                DFS(grid, row, 0);
+                DFS(grid, row, n -1);
+            }
+
+            for (int col = 0; col < n; col++)
+            {
+                DFS(grid, 0, col);
+                DFS(grid, m -1, col);
+            }
+
+            var res = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1) res++;
+                }
+            }
+
+            return res;
+        }
+
+        private static void DFS(int[][] grid, int row, int col)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == 0) return;
+
+            grid[row][col] = 0;
+
+            DFS(grid, row, col + 1);
+            DFS(grid, row, col -1);
+            DFS(grid, row + 1, col);
+            DFS(grid, row -1, col);
+
+        }
+
+    }
+}
