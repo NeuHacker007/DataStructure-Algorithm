3bfda8691ce4179e81569bed0102778feb4d84d8
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-12-04 10:22:01 -0500

    Implemented the LeetCode1905

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode1905Tests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFSSolution;
+using NUnit.Framework;
+
+namespace Solution.Tests
+{
+    [TestFixture]
+    public class LeetCode1905Tests
+    {
+        private static int[][] input1Grid1 = new[]
+        {
+            new int[] { 1, 1, 1, 0, 0 },
+            new int[] { 0, 1, 1, 1, 1 },
+            new int[] { 0, 0, 0, 0, 0 },
+            new int[] { 1, 0, 0, 0, 0 },
+            new int[] { 1, 1, 0, 1, 1 }
+        };
+
+        private static int[][] input1Grid2 = new[]
+        {
+            new int[] { 1, 1, 1, 0, 0 },
+            new int[] { 0, 0, 1, 1, 1 },
+            new int[] { 0, 1, 0, 0, 0 },
+            new int[] { 1, 0, 1, 1, 0 },
+            new int[] { 0, 1, 0, 1, 0 },
+        };
+
+        private static int[][] input2Grid1 = new[]
+        {
+            new int[] { 1,0,1,0,1 },
+            new int[] { 1,1,1,1,1 },
+            new int[] { 0,0,0,0,0 },
+            new int[] { 1,1,1,1,1 },
+            new int[] {1,0,1,0,1 }
+        };
+
+        private static int[][] input2Grid2 = new[]
+        {
+            new int[] { 0,0,0,0,0 },
+            new int[] {1,1,1,1,1 },
+            new int[] { 0,1,0,1,0 },
+            new int[] { 0,1,0,1,0 },
+            new int[] { 1,0,0,0,1 },
+        };
+
+
+        private static Object[] countSubIslands =
+        {
+            new Object[] { input1Grid1, input1Grid2, 3},
+            new Object[] { input2Grid1, input2Grid2, 2},
+
+        };
+
+        [Test]
+        [TestCaseSource("countSubIslands")]
+        public void CountSubIslands(int[][] grid1, int[][] grid2, int expectedResult)
+        {
+            var result = LeetCode1905.CountSubIslands(grid1, grid2);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.1905.Count.Sub.Islands.Medium/LeetCode1905.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFSSolution
+{
+    public class LeetCode1905
+    {
+        public static int CountSubIslands(int[][] grid1, int[][] grid2)
+        {
+            var m = grid1.Length;
+            var n = grid1[0].Length;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid1[i][j] == 0 && grid2[i][j] == 1)
+                    {
+                        DFS(grid2, i, j);
+                    }
+                }
+            }
+
+            var res = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid2[i][j] == 1)
+                    {
+                        res++;
+                        DFS(grid2, i, j);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private static void DFS(int[][] grid, int row, int col)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == 0)
+            {
+                return;
+            }
+
+            grid[row][col] = 0;
+
+            DFS(grid, row, col + 1);
+            DFS(grid, row, col -1);
+            DFS(grid, row + 1, col);
+            DFS(grid, row -1, col);
+
+
+        }
+    }
+}
