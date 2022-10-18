1199c8164d693a27077d18e1f6d6117e4b109256
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-10-18 11:20:42 -0400

    Implemented the LeetCode16

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode16.test.cs
@@ -0,0 +1,26 @@
+﻿// 
+// Project: Solution.Tests
+// Author:yifan zhang
+// Date: 10/18/2022 11:09 AM
+// Created: 10/18/2022 11:09 AM
+// FileName: LeetCode16.test.cs
+// Description:
+
+using NUnit.Framework;
+using TwoPointerSolution;
+
+namespace Solution.Tests;
+
+[TestFixture]
+public class LeetCode16_test
+{
+    [Test]
+    [TestCase(new int[] {-1,2,1,-4}, 1, 2)]
+    [TestCase(new int[] {0,0,0}, 1, 0)]
+    public void Call_Solution1_ShouldReturn_SumOf3NumsClosestToTarget(int[] nums, int target, int expectedResult)
+    {
+        int result = LeetCode16.Solution1(nums, target);
+        
+        Assert.That(result == expectedResult);
+    }
+}
