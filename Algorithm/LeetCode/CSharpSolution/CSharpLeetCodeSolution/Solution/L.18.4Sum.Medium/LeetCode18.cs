cad83730d352ce8b8a175e4246843ca1acf75a5e
Ivan Zhang <yf.eva.yifan@gmail.com>
2023-11-22 22:33:43 -0500

    Implemented the 4Sum

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.18.4Sum.Medium/LeetCode18.cs
@@ -0,0 +1,77 @@
+﻿// 
+// Project: Solution
+// Author:yifan zhang
+// Date: 11/22/2023 10:08 PM
+// Created: 11/22/2023 10:08 PM
+// FileName: LeetCode18.cs
+// Description:
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Solution.L._18._4Sum.Medium;
+
+public class LeetCode18
+{
+    public static IList<IList<int>> Solution(int[] nums, int target)
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+        int length = nums.Length;
+        int p1 = 0;
+        int p2 = 0;
+        long sum = 0;
+        Array.Sort(nums);
+        for (p1 = 0; p1 < length - 3; p1++)
+        {
+            for (p2 = p1 + 1; p2 < length - 2; p2++)
+            {
+                int left = p2 + 1;
+                int right = length - 1;
+
+                while (left < right)
+                {
+                    sum = (long)nums[p1] + (long)nums[p2] + (long)nums[left] + (long)nums[right];
+                    if (sum == target)
+                    {
+                        // add results 
+                        CheckAndAdd(result, nums[p1], nums[p2], nums[left], nums[right]);
+                        left++;
+                        right--;
+                    }
+                    else if (sum > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                }
+
+                while (p2 < length - 2 && nums[p2 + 1] == nums[p2])
+                {
+                    p2++;
+                }
+            }
+
+            while (p1 < length - 3 && nums[p1 + 1] == nums[p1])
+            {
+                p1++;
+            }
+        }
+
+
+        return result;
+    } 
+    
+    static void CheckAndAdd(IList<IList<int>> result, int a, int b, int c, int d) {
+        if (result.Count == 0) {
+            result.Add(new List<int> {a, b, c, d});
+        }
+        else if (result[result.Count-1][0] != a || result[result.Count-1][1] != b || result[result.Count-1][2] != c) {
+            result.Add(new List<int> {a, b, c, d});
+        }
+        return;
+    }
+}
--- Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/Program.cs
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using Solution;
 using DPSolution;
+using Solution.L._18._4Sum.Medium;
+
 namespace Solution
 {
     class Program
     {
         static void Main(string[] args)
         {
-            LeetCode6.Convert("PAYPALISHIRING",3);
+            int[] numbers = new[] { 1000000000, 1000000000, 1000000000, 1000000000 };
+            int target = -294967296;
+            Console.WriteLine(int.MaxValue);
+            LeetCode18.Solution(numbers, target);
         }
     }
 }
