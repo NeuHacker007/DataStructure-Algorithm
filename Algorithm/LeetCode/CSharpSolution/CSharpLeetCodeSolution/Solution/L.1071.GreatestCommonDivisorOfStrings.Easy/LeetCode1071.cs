84843a39ad6b47cc9d16d543547a455d0da5436d
Ivan Zhang <yf.eva.yifan@gmail.com>
2024-04-26 14:49:27 -0400

    Implemented the LeetCode1071.cs

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.1071.GreatestCommonDivisorOfStrings.Easy/LeetCode1071.cs
@@ -0,0 +1,34 @@
+﻿// Project: Solution
+// Author:yfeva
+// Date: 04/26/2024 14:04
+// Created: 04/26/2024 14:04
+// FileName: LeetCode1071.cs
+// Description:
+
+namespace Solution.L._1071.GreatestCommonDivisorOfStrings.Easy;
+
+public class LeetCode1071
+{
+    public static int GDC(int x, int y)
+    {
+        if (y == 0)
+        {
+            return x;
+        }
+        else
+        {
+            return GDC(y, x % y);
+        }
+    }
+    public static string GetGDCString(string str1, string str2)
+    {
+        if (!(str1 + str2).Equals((str2 + str1)))
+        {
+            return "";
+        }
+
+        int gdc = GDC(str1.Length, str2.Length);
+
+        return str1.Substring(0, gdc);
+    }
+}
