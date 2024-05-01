144dd5070c5a8e1e210062789ad341d2338ffb7d
Ivan Zhang <yf.eva.yifan@gmail.com>
2024-04-30 20:27:35 -0400

    Implemented the 125

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode125.test.cs
@@ -0,0 +1,29 @@
+﻿// Project: Solution.Tests
+// Author:yfeva
+// Date: 04/30/2024 20:04
+// Created: 04/30/2024 20:04
+// FileName: LeetCode125.test.cs
+// Description:
+
+using NUnit.Framework;
+using Solution.L._125.ValidPalinarome.Easy;
+
+namespace Solution.Tests;
+
+[TestFixture]
+public class LeetCode125_test
+{
+    [TestCase("")]
+    [TestCase("A man, a plan, a canal: Panama")]
+    public void IsPalindrome_ShouldReturnTrue(string s)
+    {
+       var result =  LeetCode125.IsPalindrome(s);
+       Assert.IsTrue(result);
+    }
+    [TestCase("race a car")]
+    public void IsPalindrome_ShouldReturnFalse(string s)
+    {
+        var result =  LeetCode125.IsPalindrome(s);
+        Assert.IsFalse(result);
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.125.ValidPalinarome.Easy/LeetCode125.cs
@@ -0,0 +1,38 @@
+﻿// Project: Solution
+// Author:yfeva
+// Date: 04/30/2024 20:04
+// Created: 04/30/2024 20:04
+// FileName: LeetCode125.cs
+// Description:
+
+namespace Solution.L._125.ValidPalinarome.Easy;
+
+public class LeetCode125
+{
+    public static bool IsPalindrome(string s)
+    {
+        int start = 0;
+        int end = s.Length - 1;
+
+        while (start < end)
+        {
+            while (start < end && !char.IsLetterOrDigit(s[start]))
+            {
+                start++;
+            }
+
+            while (start < end && !char.IsLetterOrDigit(s[end]))
+            {
+                end--;
+            }
+
+            if (char.ToLower(s[start]) != char.ToLower(s[end]))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
