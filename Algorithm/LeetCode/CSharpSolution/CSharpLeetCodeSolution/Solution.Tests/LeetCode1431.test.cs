8ba4a2c7b9b8cda323cef2d025bbf01e53aa56ef
Ivan Zhang <yf.eva.yifan@gmail.com>
2024-05-04 11:54:12 -0400

    Implemented LeetCode1431.cs

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode1431.test.cs
@@ -0,0 +1,30 @@
+﻿// Project: Solution.Tests
+// Author:yfeva
+// Date: 05/04/2024 11:05
+// Created: 05/04/2024 11:05
+// FileName: LeetCode1431.test.cs
+// Description:
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using Solution.L._1431.KidsWithTheGreatestNumberOfCandies.Easy;
+
+namespace Solution.Tests;
+
+[TestFixture]
+public class LeetCode1431_test
+{
+    [TestCase(new int[] {2,3,5,1,3}, 3, new bool[]{true,true,true,false, true})]
+    [TestCase(new int[] {4,2,1,1,2}, 1, new bool[]{true,false,false,false, false})]
+    [TestCase(new int[] {12,1,12}, 10, new bool[]{true,false,true})]
+    public void Leecode1431_KidsWithCandies_ShouldReturnCorrectBooleanList(
+        int[] candies, 
+        int extraCandies, 
+        bool[] expectedResult)
+    {
+
+        var result = LeetCode1431.KidsWithCandies(candies, extraCandies);
+        
+        CollectionAssert.AreEqual(expectedResult, result);
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.1431.KidsWithTheGreatestNumberOfCandies.Easy/LeetCode1431.cs
@@ -0,0 +1,26 @@
+﻿// Project: Solution
+// Author:yfeva
+// Date: 05/04/2024 11:05
+// Created: 05/04/2024 11:05
+// FileName: LeetCode1431.cs
+// Description:
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution.L._1431.KidsWithTheGreatestNumberOfCandies.Easy;
+
+public class LeetCode1431
+{
+    public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        int maxCandies = candies.Max();
+        IList<bool> results = new List<bool>(candies.Length);
+        foreach (var candy in candies)
+        {
+            results.Add(candy + extraCandies >= maxCandies);
+        }
+        
+        return results;
+    }
+}
