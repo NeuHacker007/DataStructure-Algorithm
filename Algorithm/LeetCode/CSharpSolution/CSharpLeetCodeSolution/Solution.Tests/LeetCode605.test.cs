c4892e8fd5dcd6ad81d58832a0ef14da96a2bbbc
Ivan Zhang <yf.eva.yifan@gmail.com>
2024-05-05 09:11:35 -0400

    Implemented LeetCode605.cs

---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution.Tests/LeetCode605.test.cs
@@ -0,0 +1,24 @@
+﻿// Project: Solution.Tests
+// Author:yfeva
+// Date: 05/04/2024 12:05
+// Created: 05/04/2024 12:05
+// FileName: LeetCode605.test.cs
+// Description:
+
+using NUnit.Framework;
+using Solution.L._605.CanPlaceFlower.Easy;
+
+namespace Solution.Tests;
+
+[TestFixture]
+public class LeetCode605_test
+{
+    [TestCase]
+    public void Leecode605_()
+    {
+
+        //var result = LeetCode605.CanPlaceFlower2(new int[] {1,0,0,0,0,0,1}, 2);
+        
+        // Assert.IsTrue(result);
+    }
+}
---
+++ Algorithm/LeetCode/CSharpSolution/CSharpLeetCodeSolution/Solution/L.605.CanPlaceFlower.Easy/LeetCode605.cs
@@ -0,0 +1,40 @@
+﻿// Project: Solution
+// Author:yfeva
+// Date: 05/04/2024 12:05
+// Created: 05/04/2024 12:05
+// FileName: LeetCode605.cs
+// Description:
+
+namespace Solution.L._605.CanPlaceFlower.Easy;
+
+public class LeetCode605
+{
+    public static bool CanPlaceFlower(int[] flowerbed, int n)
+    {
+        int placesCanBePlaced = 0;
+
+        for(int i = 0; i < flowerbed.Length; i++) {
+
+            if (flowerbed[i] == 0) {
+                bool isLeftCanPlace = i == 0 || flowerbed[i-1] == 0;
+                bool isRightCanPlace = i == flowerbed.Length - 1 || flowerbed[i+1] == 0;
+
+                if (isLeftCanPlace && isRightCanPlace ) {
+                    flowerbed[i] = 1;
+                    placesCanBePlaced++;
+                }
+            }
+            
+        }
+
+        return placesCanBePlaced >= n;
+    }
+
+    public static bool CanPlaceFlower2(int[] flowerBed, int n)
+    {
+        // DP[i] represents number of pots can be fufilled 
+
+
+        return false;
+    }
+}
